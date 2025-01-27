6c7ef6095f009fe51103995b0efcc33988f4effe
lee_jun_young <ljy03@pusan.ac.kr>
2025-01-27 14:57:55 +0900

    1/27 JS 학습

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-alert("hi")
+const loginForm = document.getElementById("login-form");
+const loginInput = loginForm.querySelector("input");
+const loginButtin = loginForm.querySelector("button");
+
+// const loginInput = document.querySelector("#login-form input")
+// const loginButton = document.querySelector("#login-form button")
+
+function onLoginBtnClick() {
+    console.log(loginInput.value)
+}
+
+loginButtin.addEventListener("click", onLoginBtnClick);
